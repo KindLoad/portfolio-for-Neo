eca4e9359541bd2628672aad16a256e3b70f6ead
kdge <kdge248@gmail.com>
2020-07-14 15:35:51 +0900

    multichat

---
+++ VS_Server/Chat_Multi_Client/Chat_Multi_Client/Chat_Multi_Client.cpp
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <WinSock2.h>
+#include <string>
+
+#pragma comment(lib, "ws2_32")
+
+#define PORT 4578
+#define PACKET_SIZE 1024
+#define SERVER_IP "172.17.10.61"
+
+using namespace std;
+
+int main() {
+	//준비 단계
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	// 보내는 소켓 생성
+	SOCKET SendRecvSocekt = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	//서버 정보를 입력
+	SOCKADDR_IN tAddr = {};
+	tAddr.sin_family = AF_INET;
+	tAddr.sin_port = htons(PORT);
+	tAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+	//연결
+	connect(SendRecvSocekt, (SOCKADDR*)&tAddr, sizeof(tAddr));
+
+	while (1) {
+		char buf[PACKET_SIZE];
+		printf(">> ");
+		scanf(" %[^\n]", buf);
+		printf(" ");
+
+		//메세지 전송
+		int SendSize = send(SendRecvSocekt, buf, strlen(buf), 0);
+		int RecvSize = recv(SendRecvSocekt, buf, sizeof(buf), 0);
+		
+		buf[RecvSize] = '\0';
+		//printf("server >> %s\n", buf);
+	}
+	closesocket(SendRecvSocekt);
+
+	WSACleanup();
+	return 0;
+}
---
+++ VS_Server/Chat_Multi_Server/Chat_Multi_Server/Chat_Multi_Server.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <Windows.h>
+#include <thread>
+#include <cstring>
+#include <string>
+#include <vector>
+
+#define PACKET_SIZE 1024
+#define PORT 4578
+
+#pragma comment (lib , "ws2_32.lib")
+
+using namespace std;
+
+void __cdecl Recvthread(void* p) {
+	SOCKET sock = (SOCKET)p;
+	char buf[PACKET_SIZE];
+	
+    while (true) {
+        //수신
+        int recvsize = recv(sock,buf, sizeof(buf), 0);
+        if (recvsize <= 0)
+        {
+            printf("connect end\n");
+            break;
+        }
+
+        buf[recvsize] = '\0'; //문장 끝 지정
+        printf("client >> %s\n", buf);
+
+        //전송
+        int sendsize = send(sock, buf, strlen(buf), 0);
+        if (sendsize <= 0) break;
+    }
+    closesocket(sock);
+}
+
+int main()
+{
+	//준비 과정
+	WSADATA wsaData;
+	char buf[PACKET_SIZE];
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	//받는 소켓 지정
+	SOCKET RecvSocket; // 받는 소켓(연결용)
+	RecvSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+
+	//서버에 소켓 정보를 입력
+	SOCKADDR_IN ListenAddr = {}; //소켓 연결 클래스
+	ListenAddr.sin_family = AF_INET;
+	ListenAddr.sin_port = htons(PORT);//조심
+	ListenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	//연결
+	bind(RecvSocket, (SOCKADDR*)&ListenAddr, sizeof(ListenAddr)); //bind 연결
+	listen(RecvSocket, 5); //5인만 대기
+
+	SOCKADDR_IN Client_Addr = { 0 };
+	int Client_size = sizeof(Client_Addr);
+
+	while (1) {
+		SOCKET Client_sock = accept(RecvSocket, (SOCKADDR*)&Client_Addr, &Client_size);
+		printf("클라이언트 접속\n");
+		printf("IP : %s, 포트 : %d, \n",inet_ntoa(Client_Addr.sin_addr), Client_Addr.sin_port);
+		_beginthread(Recvthread, NULL, (void*)Client_sock);
+	}
+	closesocket(RecvSocket);
+
+	WSACleanup();
+	return 0;
+
+
+}
+
